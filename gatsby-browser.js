d5ca05c14970d2a2baab3b66e065dc6cb092451e
Đorđe Savić <djordje.savic@tobonet.com>
2020-08-15 20:10:45 +0200

    prefix env var

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,7 @@ const client = new ApolloClient({
   uri: "https://graphql.fauna.com/graphql",
   cache: new InMemoryCache(),
   headers: {
-    authorization: `Bearer ${process.env.FAUNADB_SERVER_SECRET}`,
+    authorization: `Bearer ${process.env.GATSBY_FAUNADB_SERVER_SECRET}`,
   },
 });
 
